46ebbf7838a88d928d60513d2c1f050db99f92ef
Erdem <erdemdurmaz7>
2021-02-06 18:32:14 +0100

    1.0 changes

---
+++ The Collatz Sequence.py
@@ -0,0 +1,25 @@
+#page 101 The Collatz Sequence
+def collatz(number):
+    if number % 2 == 0:
+        print("This number",number," is even")
+        floor = number // 2
+        print(floor)
+        if floor == 1:
+            print(" Collatz sequence is initiated")
+            quit()
+        
+    else:
+        print("This number",number," is odd")
+        calc = 3 * number + 1
+        print(calc)
+        if calc == 1:
+            print(" Collatz sequence is initiated")
+            quit()
+        
+
+while True:
+    try:
+        usrinp = int(input("Enter the number: "))
+    except:
+        print("Something went wrong")
+    collatz(usrinp)
